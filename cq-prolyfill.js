4c77227fbbb29c3a9a8702422863c57eb94affde
Martin Auswöger <martin@auswoeger.com>
2015-08-01 16:11:43 +0200

    Add license

--- cq-prolyfill.js
+++ cq-prolyfill.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright Martin Auswöger
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 (function(window, document) {
 'use strict';
 
