dbcc062a14e51ccd7837b5e795647741d3184c9b
Martin Auswöger <martin@auswoeger.com>
2015-08-07 18:50:18 +0200

    Fix test descriptions

--- tests.js
+++ tests.js
@@ -39,37 +39,37 @@ QUnit.test('Simple width and height Query', function(assert) {
 
 		element.style.cssText = 'width: 109px; height: 109px';
 		reevaluate();
-		assert.equal(font(minW), 'no-query', 'min-width at 109px');
-		assert.equal(font(minH), 'no-query', 'min-height at 109px');
+		assert.equal(font(minW), 'no-query', 'min-width at 99px');
+		assert.equal(font(minH), 'no-query', 'min-height at 99px');
 
 		element.style.cssText = 'width: 110px; height: 110px';
 		reevaluate();
-		assert.equal(font(minW), 'min-width-100', 'min-width at 110px');
-		assert.equal(font(minH), 'min-height-100', 'min-height at 110px');
-		assert.equal(font(maxW), 'max-width-100', 'max-width at 110px');
-		assert.equal(font(maxH), 'max-height-100', 'max-height at 110px');
+		assert.equal(font(minW), 'min-width-100', 'min-width at 100px');
+		assert.equal(font(minH), 'min-height-100', 'min-height at 100px');
+		assert.equal(font(maxW), 'max-width-100', 'max-width at 100px');
+		assert.equal(font(maxH), 'max-height-100', 'max-height at 100px');
 
 		element.style.cssText = 'width: 111px; height: 111px';
 		reevaluate();
-		assert.equal(font(maxW), 'max-width-200', 'max-width at 111px');
-		assert.equal(font(maxH), 'max-height-200', 'max-height at 111px');
+		assert.equal(font(maxW), 'max-width-200', 'max-width at 101px');
+		assert.equal(font(maxH), 'max-height-200', 'max-height at 101px');
 
 		element.style.cssText = 'width: 209px; height: 209px';
 		reevaluate();
-		assert.equal(font(minW), 'min-width-100', 'min-width at 209px');
-		assert.equal(font(minH), 'min-height-100', 'min-height at 209px');
+		assert.equal(font(minW), 'min-width-100', 'min-width at 199px');
+		assert.equal(font(minH), 'min-height-100', 'min-height at 199px');
 
 		element.style.cssText = 'width: 210px; height: 210px';
 		reevaluate();
-		assert.equal(font(maxW), 'max-width-200', 'max-width at 210px');
-		assert.equal(font(maxH), 'max-height-200', 'max-height at 210px');
-		assert.equal(font(minW), 'min-width-200', 'min-width at 210px');
-		assert.equal(font(minH), 'min-height-200', 'min-height at 210px');
+		assert.equal(font(maxW), 'max-width-200', 'max-width at 200px');
+		assert.equal(font(maxH), 'max-height-200', 'max-height at 200px');
+		assert.equal(font(minW), 'min-width-200', 'min-width at 200px');
+		assert.equal(font(minH), 'min-height-200', 'min-height at 200px');
 
 		element.style.cssText = 'width: 211px; height: 211px';
 		reevaluate();
-		assert.equal(font(maxW), 'no-query', 'max-width at 211px');
-		assert.equal(font(maxH), 'no-query', 'max-height at 211px');
+		assert.equal(font(maxW), 'no-query', 'max-width at 201px');
+		assert.equal(font(maxH), 'no-query', 'max-height at 201px');
 
 
 		document.head.removeChild(style.previousSibling);
