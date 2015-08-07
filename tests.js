5c85e4b0c982f8b1f2630df170474c1834297cee
Martin Auswöger <martin@auswoeger.com>
2015-08-07 18:35:36 +0200

    Functional test of simple width and height queries

--- tests.js
+++ tests.js
@@ -2,6 +2,84 @@
 (function() {
 'use strict';
 
+/*global reprocess, reevaluate*/
+QUnit.test('Simple width and height Query', function(assert) {
+
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = '.minW, .maxW, .minH, .maxH { font-family: no-query }'
+		+ '.minW:container( min-width: 100px ) { font-family: min-width-100 }'
+		+ '.minW:container( min-width: 200px ) { font-family: min-width-200 }'
+		+ '.minH:container( min-height: 100px ) { font-family: min-height-100 }'
+		+ '.minH:container( min-height: 200px ) { font-family: min-height-200 }'
+		+ '.maxW:container( max-width: 200px ) { font-family: max-width-200 }'
+		+ '.maxW:container( max-width: 100px ) { font-family: max-width-100 }'
+		+ '.maxH:container( max-height: 200px ) { font-family: max-height-200 }'
+		+ '.maxH:container( max-height: 100px ) { font-family: max-height-100 }';
+	document.head.appendChild(style);
+
+	var element = document.createElement('div');
+	element.innerHTML = '<div style="padding: 5px; height: 100%; box-sizing: border-box"><span><div style="float: left"><span>'
+		+ '<div class="maxW"></div>'
+		+ '<div class="minW"></div>'
+		+ '<div class="maxH"></div>'
+		+ '<div class="minH"></div>';
+	document.body.appendChild(element);
+	var minW = element.querySelector('.minW');
+	var maxW = element.querySelector('.maxW');
+	var minH = element.querySelector('.minH');
+	var maxH = element.querySelector('.maxH');
+
+	var done = assert.async();
+	reprocess(function () {
+
+		var font = function(node) {
+			return window.getComputedStyle(node).fontFamily;
+		};
+
+		element.style.cssText = 'width: 109px; height: 109px';
+		reevaluate();
+		assert.equal(font(minW), 'no-query', 'min-width at 109px');
+		assert.equal(font(minH), 'no-query', 'min-height at 109px');
+
+		element.style.cssText = 'width: 110px; height: 110px';
+		reevaluate();
+		assert.equal(font(minW), 'min-width-100', 'min-width at 110px');
+		assert.equal(font(minH), 'min-height-100', 'min-height at 110px');
+		assert.equal(font(maxW), 'max-width-100', 'max-width at 110px');
+		assert.equal(font(maxH), 'max-height-100', 'max-height at 110px');
+
+		element.style.cssText = 'width: 111px; height: 111px';
+		reevaluate();
+		assert.equal(font(maxW), 'max-width-200', 'max-width at 111px');
+		assert.equal(font(maxH), 'max-height-200', 'max-height at 111px');
+
+		element.style.cssText = 'width: 209px; height: 209px';
+		reevaluate();
+		assert.equal(font(minW), 'min-width-100', 'min-width at 209px');
+		assert.equal(font(minH), 'min-height-100', 'min-height at 209px');
+
+		element.style.cssText = 'width: 210px; height: 210px';
+		reevaluate();
+		assert.equal(font(maxW), 'max-width-200', 'max-width at 210px');
+		assert.equal(font(maxH), 'max-height-200', 'max-height at 210px');
+		assert.equal(font(minW), 'min-width-200', 'min-width at 210px');
+		assert.equal(font(minH), 'min-height-200', 'min-height at 210px');
+
+		element.style.cssText = 'width: 211px; height: 211px';
+		reevaluate();
+		assert.equal(font(maxW), 'no-query', 'max-width at 211px');
+		assert.equal(font(maxH), 'no-query', 'max-height at 211px');
+
+
+		document.head.removeChild(style.previousSibling);
+		document.head.removeChild(style);
+		document.body.removeChild(element);
+		done();
+
+	});
+});
+
 /*global preprocess, SELECTOR_ESCAPED_REGEXP*/
 QUnit.test('preprocess', function(assert) {
 	var style = document.createElement('style');
