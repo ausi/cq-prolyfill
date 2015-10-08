ad6dc95bf833c3e273311b7019af80c47083dabe
Martin Auswöger <martin@auswoeger.com>
2015-10-08 16:08:31 +0200

    PostCSS plugin, close #2

---
+++ postcss-plugin.js
@@ -0,0 +1,19 @@
+/*eslint-env node */
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('cq-prolyfill', function () {
+	'use strict';
+	return function (css) {
+		css.walkRules(/:container\(/i, function (rule) {
+			rule.selectors = rule.selectors.map(function(selector) {
+				return selector.replace(/:container\([^)]*\)/gi, function(match) {
+					return '.' + match
+						.replace(/([a-z])\s+([a-z])/gi, '$1|$2')
+						.replace(/\s+/g, '')
+						.replace(/[[\]!"#$%&'()*+,./:;<=>?@^`{|}~]/g, '\\$&')
+						.toLowerCase();
+				});
+			});
+		});
+	};
+});
